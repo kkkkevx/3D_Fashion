77d5e1cdcb76b7583e0d13e618b4e95d185e7877
kkkkevx <kanwen7788@gmail.com>
2023-09-14 12:09:47 -0700

    add comment to home-content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,52 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 import state from "../store";
-import {  slideAnimation,
+import {
+  slideAnimation,
   headTextAnimation,
   headContentAnimation,
-  headContainerAnimation} 
-from "../config/motion"
-
+  headContainerAnimation,
+} from "../config/motion";
 
 const Home = () => {
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
 
-  
   return (
     <AnimatePresence>
+      {/* Check if 'snap.intro' is true before rendering */}
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
           <motion.header {...slideAnimation("down")}>
-            <img src="..\threejs.png" 
-            alt="logo" 
-            className="w-8 h-8 object-contain"
+            <img
+              src="..\threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
             />
           </motion.header>
 
+          {/* Apply animations to the home-content */}
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden"/> DO IT.
+                LET'S <br className="xl:block hidden" /> DO IT.
               </h1>
             </motion.div>
-            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
-              <p className="max-w-md font-normal text-gray-600">Create your unique and exclusive shirt with our brand-new 3D customization tooll.
-                <strong>Unleash your imagination</strong> {" "} and define your own style.
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600">
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tooll.
+                <strong>Unleash your imagination</strong> and define your own
+                style.
               </p>
             </motion.div>
           </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
 export default Home;
