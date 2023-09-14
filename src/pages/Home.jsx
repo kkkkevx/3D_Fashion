ab9c002daac3b3c77a20690621db9aa128ff4b43
kkkkevx <kanwen7788@gmail.com>
2023-09-14 11:48:12 -0700

    add basic text about home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,12 +16,25 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
-          <motion.header>
+          <motion.header {...slideAnimation("down")}>
             <img src="..\threejs.png" 
             alt="logo" 
             className="w-8 h-8 object-contain"
             />
           </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden"/> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600">Create your unique and exclusive shirt with our brand-new 3D customization tooll.
+                <strong>Unleash your imagination</strong> {" "} and define your own style.
+              </p>
+            </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
